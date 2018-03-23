37818b65d16855e4cc826df23d1b4d9f95e180ae
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2018-03-23 21:47:20 +0900

    micropython pin 지원 삭제

--- src/modmachine.h
+++ src/modmachine.h
@@ -3,18 +3,5 @@
 
 #include "umport_mcu.h"
 
-typedef struct _machine_pin_obj_t {
-    mp_obj_base_t base;
-    qstr name;
-    gpio_t *port;
-    mp_uint_t pin;
-} machine_pin_obj_t;
-
-extern const mp_obj_type_t machine_pin_type;
-
-machine_pin_obj_t *machine_pin_get(mp_obj_t *obj_in);
-void pin_set(mp_obj_t self_in, int value);
-
-extern const mp_obj_type_t machine_hard_i2c_type;
 
 #endif

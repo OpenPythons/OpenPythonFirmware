7fbbfe48d6ff93ae91eafec7cd5966b7019eb84e
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2018-03-23 21:49:12 +0900

    umport/umport_mcu.h 코드 정리

--- src/umport_mcu.h
+++ src/umport_mcu.h
@@ -13,6 +13,7 @@ typedef struct _umport_controller_t {
 
 #define UMPORT_CONTROLLER ((umport_controller_t*)0x40000100)
 
+
 typedef struct _gpio_t {
     volatile uint32_t ODR;
     volatile uint32_t IDR;
@@ -22,12 +23,14 @@ typedef struct _gpio_t {
 #define GPIO_X ((gpio_t*)0x40000208)
 #define GPIO_Y ((gpio_t*)0x40000210)
 
+
 typedef struct _rtc_t {
     volatile uint32_t TICKS_MS;
     volatile uint32_t TICKS_US;
 } rtc_t;
 
 #define RTC ((rtc_t*)0x40000300)
+
 
 typedef struct _ucp_uart_t {
     volatile uint32_t TXR;

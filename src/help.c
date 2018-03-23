6c5d361c3280540e8db161f8653e5a4aa183b7fb
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2018-03-23 22:09:05 +0900

    umport/help 텍스트 몇 변수명 변경

--- src/help.c
+++ src/help.c
@@ -1,14 +1,7 @@
 #include "py/builtin.h"
 
-const char *unicorn_help_text =
-"Welcome to MicroPython running under Unicorn!\n"
-"\n"
-"Control commands:\n"
-"  CTRL-A        -- on a blank line, enter raw REPL mode\n"
-"  CTRL-B        -- on a blank line, enter normal REPL mode\n"
-"  CTRL-C        -- interrupt a running program\n"
-"  CTRL-D        -- on a blank line, do a soft reset of the board\n"
-"  CTRL-E        -- on a blank line, enter paste mode\n"
+const char umport_help_text[] =
+"Welcome to MicroPython running under umport!\n"
 "\n"
 "For further help on a specific object, type help(obj)\n"
 #if MICROPY_PY_BUILTINS_HELP_MODULES

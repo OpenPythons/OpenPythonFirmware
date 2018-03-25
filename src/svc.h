4fb466af639793b198fd82f64f075c12890a17c7
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2018-03-25 17:54:41 +0900

    svc side-effect 버그 수정 어셈블리 입/출력 인수 제대로 수정함

--- src/svc.h
+++ src/svc.h
@@ -7,93 +7,94 @@
 #define R2 R[2]
 #define R3 R[3]
 
-#define SVC4(code, a, b, c, d) __asm__ volatile ( \
-    "mov r0, %0\n\t" \
-    "mov r1, %1\n\t" \
-    "mov r2, %2\n\t" \
-    "mov r3, %3\n\t" \
+
+#define SVC4(code, a, b, c, d) __asm__ ( \
+    "mov r0, %4\n\t" \
+    "mov r1, %5\n\t" \
+    "mov r2, %6\n\t" \
+    "mov r3, %7\n\t" \
     "svc %[imm]\n\t" \
-    "str r0, %4\n\t" \
-    "str r1, %5\n\t" \
-    "str r2, %6\n\t" \
-    "str r3, %7\n\t" \
-    :"+r" (a), \
-     "+r" (b), \
-     "+r" (c), \
-     "+r" (d) \
-    :"m" (R[0]), \
-     "m" (R[1]), \
-     "m" (R[2]), \
-     "m" (R[3]), \
+    "str r0, %0\n\t" \
+    "str r1, %1\n\t" \
+    "str r2, %2\n\t" \
+    "str r3, %3\n\t" \
+    :"=m" (R0), \
+     "=m" (R1), \
+     "=m" (R2), \
+     "=m" (R3) \
+    :"r" (a), \
+     "r" (b), \
+     "r" (c), \
+     "r" (d), \
      [Z] "I" (0), \
      [imm] "I" (code) \
     :"r0", "r1", "r2", "r3" \
 );
 
-#define SVC3(code, a, b, c) __asm__ volatile ( \
-    "mov r0, %0\n\t" \
-    "mov r1, %1\n\t" \
-    "mov r2, %2\n\t" \
+#define SVC3(code, a, b, c) __asm__ ( \
+    "mov r0, %4\n\t" \
+    "mov r1, %5\n\t" \
+    "mov r2, %6\n\t" \
     "mov r3, %[Z]\n\t" \
     "svc %[imm]\n\t" \
-    "str r0, %3\n\t" \
-    "str r1, %4\n\t" \
-    "str r2, %5\n\t" \
-    "str r3, %6\n\t" \
-    :"+r" (a), \
-     "+r" (b), \
-     "+r" (c), \
-    :"m" (R[0]), \
-     "m" (R[1]), \
-     "m" (R[2]), \
-     "m" (R[3]), \
+    "str r0, %0\n\t" \
+    "str r1, %1\n\t" \
+    "str r2, %2\n\t" \
+    "str r3, %3\n\t" \
+    :"=m" (R0), \
+     "=m" (R1), \
+     "=m" (R2), \
+     "=m" (R3) \
+    :"r" (a), \
+     "r" (b), \
+     "r" (c), \
      [Z] "I" (0), \
      [imm] "I" (code) \
-    :"r0", "r1", "r2", "r3" \
+    :"r0", "r1", "r2", "r3", "memory" \
 );
 
-#define SVC2(code, a, b) __asm__ volatile ( \
-    "mov r0, %0\n\t" \
-    "mov r1, %1\n\t" \
+#define SVC2(code, a, b) __asm__ ( \
+    "mov r0, %4\n\t" \
+    "mov r1, %5\n\t" \
     "mov r2, %[Z]\n\t" \
     "mov r3, r2\n\t" \
     "svc %[imm]\n\t" \
-    "str r0, %2\n\t" \
-    "str r1, %3\n\t" \
-    "str r2, %4\n\t" \
-    "str r3, %5\n\t" \
-    :"+r" (a), \
-     "+r" (b) \
-    :"m" (R[0]), \
-     "m" (R[1]), \
-     "m" (R[2]), \
-     "m" (R[3]), \
+    "str r0, %0\n\t" \
+    "str r1, %1\n\t" \
+    "str r2, %2\n\t" \
+    "str r3, %3\n\t" \
+    :"=m" (R0), \
+     "=m" (R1), \
+     "=m" (R2), \
+     "=m" (R3) \
+    :"r" (a), \
+     "r" (b), \
      [Z] "I" (0), \
      [imm] "I" (code) \
-    :"r0", "r1", "r2", "r3" \
+    :"r0", "r1", "r2", "r3", "memory" \
 );
 
-#define SVC1(code, a) __asm__ volatile ( \
-    "mov r0, %0\n\t" \
+#define SVC1(code, a) __asm__ ( \
+    "mov r0, %4\n\t" \
     "mov r1, %[Z]\n\t" \
     "mov r2, r1\n\t" \
     "mov r3, r1\n\t" \
     "svc %[imm]\n\t" \
-    "str r0, %1\n\t" \
-    "str r1, %2\n\t" \
-    "str r2, %3\n\t" \
-    "str r3, %4\n\t" \
-    :"+r" (a) \
-    :"m" (R[0]), \
-     "m" (R[1]), \
-     "m" (R[2]), \
-     "m" (R[3]), \
+    "str r0, %0\n\t" \
+    "str r1, %1\n\t" \
+    "str r2, %2\n\t" \
+    "str r3, %3\n\t" \
+    :"=m" (R0), \
+     "=m" (R1), \
+     "=m" (R2), \
+     "=m" (R3) \
+    :"r" (a), \
      [Z] "I" (0), \
      [imm] "I" (code) \
-    :"r0", "r1", "r2", "r3" \
+    :"r0", "r1", "r2", "r3", "memory" \
 );
 
-#define SVC0(code) __asm__ volatile ( \
+#define SVC0(code) __asm__ ( \
     "mov r0, %[Z]\n\t" \
     "mov r1, r0\n\t" \
     "mov r2, r0\n\t" \
@@ -103,14 +104,13 @@
     "str r1, %1\n\t" \
     "str r2, %2\n\t" \
     "str r3, %3\n\t" \
-    : \
-    :"m" (R[0]), \
-     "m" (R[1]), \
-     "m" (R[2]), \
-     "m" (R[3]), \
-     [Z] "I" (0), \
+    :"=m" (R0), \
+     "=m" (R1), \
+     "=m" (R2), \
+     "=m" (R3) \
+    :[Z] "I" (0), \
      [imm] "I" (code) \
-    :"r0", "r1", "r2", "r3" \
+    :"r0", "r1", "r2", "r3", "memory" \
 );
 
 #endif

900014cf043149e6feef20327f52a649f1663279
EcmaXp <ecmaxp@ecmaxp.pe.kr>
2018-10-06 15:07:42 +0900

    do_str를 oprom/main.c 에 추가

--- src/main.c
+++ src/main.c
@@ -22,6 +22,20 @@ void debug_printer(void *self, const char *buf, size_t len) {
 
 const struct _mp_print_t debug_print = {NULL, debug_printer};
 
+void do_str(const char *src, mp_parse_input_kind_t input_kind) {
+    nlr_buf_t nlr;
+    if (nlr_push(&nlr) == 0) {
+        mp_lexer_t *lex = mp_lexer_new_from_str_len(MP_QSTR__lt_stdin_gt_, src, strlen(src), 0);
+        mp_parse_tree_t parse_tree = mp_parse(lex, input_kind);
+        mp_obj_t module_fun = mp_compile(&parse_tree, MP_QSTR__lt_stdin_gt_, MP_EMIT_OPT_NONE, false);
+        mp_call_function_0(module_fun);
+        nlr_pop();
+    } else {
+        // uncaught exception
+        mp_obj_print_exception(&mp_plat_print, (mp_obj_t) nlr.ret_val);
+    }
+}
+
 int main(int argc, char **argv) {
     nlr_buf_t nlr;
     int code;
